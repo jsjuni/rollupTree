a283c2c4c9ad110df4d6dee4b75674a860f0942d
Steven Jenkins <sjenkins@studioj.us>
2025-01-13 10:37:03 -0500

    almost ready for release

--- R/rollup.R
+++ R/rollup.R
@@ -1,34 +1,37 @@
-#' Perform recursive analysis
+#' Perform recursive computation
 #'
-#' @description `rollup()` traverses a tree in depth-first postorder and calls a
-#' user-specified update method on each vertex, passing the method a data set, the key of
-#' that vertex in the data set, and a list of child keys. The
-#' update method returns the updated data set. The final result reflects an update at
-#' every non-leaf vertex of the tree.
+#' @description `rollup()` traverses a tree depth-first (post order) and calls a
+#'   user-specified update function at each vertex, passing the method a data set,
+#'   the unique key of that target vertex in the data set, and a list of source
+#'   keys. The update method typically gets some properties of the source
+#'   elements of the data set, combines them, sets some properties of the
+#'   target element of the data set to the combined value, and returns the
+#'   updated data set as input to the update of the next vertex. The final
+#'   operation updates the root vertex.
 #'
-#' An `update_prop()` helper function is available to simplify building
-#' compliant update methods.
+#'   An `update_prop()` helper function is available to simplify building
+#'   compliant update methods.
 #'
-#' Before beginning the traversal, `rollup()` calls a user-specified method
-#' to validate that the tree is
-#' well-formed (see [default_validate_tree()]). It also calls a user-specified
-#' method to ensure that the data set
-#' the id sets of the tree and data set are identical, and that data set
-#' elements corresponding to leaf vertices in the tree satisfy some
-#' user-specified predicate, e.g., `is.numeric()`.
+#'   Before beginning the traversal, `rollup()` calls a user-specified method to
+#'   validate that the tree is well-formed (see [default_validate_tree()]). It
+#'   also calls a user-specified method to ensure that the id sets of the tree
+#'   and data set are identical, and that data set elements corresponding to
+#'   leaf vertices in the tree satisfy some user-specified predicate, e.g.,
+#'   `is.numeric()`.
 #'
-#' @details
-#' The data set passed to `rollup()` can be any object for which an `update()`
-#' method can be written. A common and simple example is a data frame, but
+#' @details The data set passed to `rollup()` can be any object for which an
+#'   `update()` method can be written. A common and simple example is a data
+#'   frame, but lists work as well.
 #'
-#' @param tree An igraph directed graph that is a valid single-rooted in-tree
-#' @param ds A data set to be updated; can be any object
-#' @param update The update method called at each vertex as update(ds,
+#' @param tree `igraph` directed graph that is a valid single-rooted in-tree
+#'   and whose vertex names are keys from the data set
+#' @param ds data set to be updated; can be any object
+#' @param update function called at each vertex as update(ds,
 #'   parent_key, child_keys)
-#' @param validate_ds The data set validator called as validate_ds(tree, ds)
-#' @param validate_tree The tree validator called as validate_tree(tree)
+#' @param validate_ds data set validator function called as validate_ds(tree, ds)
+#' @param validate_tree tree validator function called as validate_tree(tree)
 #'
-#' @return The updated data set
+#' @returns updated input data set
 #' @export
 #'
 #' @examples
@@ -55,16 +58,14 @@ rollup <- function(tree, ds, update, validate_ds, validate_tree = default_valida
 #' Update a rollup from a single leaf vertex
 #'
 #' @description
-#' update_rollup() performs a minimal update of a data set assuming a single leaf element property
+#' `update_rollup()` performs a minimal update of a data set assuming a single leaf element property
 #' has changed. It performs updates along the path from that vertex to the root. There should be no difference
-#' in the output from calling rollup() again. update_rollup() is perhaps more efficient and useful in an interactive context.
+#' in the output from calling `rollup()` again. `update_rollup()` is perhaps more efficient and useful in an interactive context.
 #'
-#' @param tree An igraph directed graph that is a valid single-rooted in-tree
-#' @param ds A data set to be updated; can be any object
+#' @inheritParams rollup
 #' @param vertex The start vertex
-#' @param update The update method called at each vertex as update(ds, parent_key, child_keys)
 #'
-#' @return The updated data set.
+#' @returns updated input data set
 #' @export
 #'
 #' @examples
@@ -90,15 +91,14 @@ update_rollup <- function(tree, ds, vertex, update) {
 #'
 #' @description
 #' `validate_ds()` ensures that a data set contains the same identifiers as a specified tree and that
-#' elements of the data set corresponding to leaf verticies in the tree satisfy a user-specified predicate.
+#' elements of the data set corresponding to leaf vertices in the tree satisfy a user-specified predicate.
 #'
-#' @param tree The tree to check against
-#' @param ds The data set to validate
-#' @param get_keys Method get keys of the data set called as get_keys(ds)
-#' @param get_prop Method to get the property to validate for leaf element with id l, called as get_prop(ds, l)
-#' @param op Logical method to test return value of get_prop() (default `is.numeric()`); returns TRUE if OK
+#' @inheritParams rollup
+#' @param get_keys function to get keys of the data set called as `get_keys(ds)`
+#' @param get_prop function to get the property value to validate for leaf element with id `l`, called as `get_prop(ds, l)`
+#' @param op logical function to test return value of `get_prop()` (default `is.numeric()`); returns `TRUE` if OK
 #'
-#' @return TRUE if validation succeeds, halts otherwise
+#' @returns TRUE if validation succeeds, halts otherwise
 #' @export
 #'
 #' @examples
@@ -120,14 +120,13 @@ validate_ds <- function(tree, ds, get_keys, get_prop, op=function(x) is.numeric(
 #' loop-free, single-edged, connected, directed, and single-rooted with edge
 #' direction from child to parent.
 #'
-#' @param tree An igraph directed graph to be validated
+#' @inheritParams rollup
 #'
-#' @return single root element if tree is valid; stops otherwise
+#' @returns single root vertex identifier if tree is valid; stops otherwise
 #' @export
 #'
 #' @examples
 #' default_validate_tree(wbs_tree)
-#'
 default_validate_tree <- function(tree) {
   if (any(igraph::which_multiple(tree))) stop("graph contains multiple edges")
   if (!igraph::is_forest(tree, mode = "all")) stop("graph is cyclic")
